8895a32110a76c13b23b6e0513c665d44b3ca593
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-12-14 07:56:41 -0600

    Started working on day4 solution

---
+++ day4/solution.go
@@ -0,0 +1,35 @@
+package day4
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Solution(input string){
+  finput := strings.Split(input, "\n")
+  
+  var sum int = 0
+  for i := 0; i < len(finput)-1; i++ {
+    elves := strings.FieldsFunc(string(finput[i]), Split)
+    var list [4]int
+    for j, val := range elves {
+      num, err := strconv.Atoi(val)
+      if err != nil {
+        panic(err)
+      }
+      list[j] = num
+    }
+
+    if list[0] >= list[2] && list[1] <= list[3] {
+      sum = sum + 1
+    } else if list[0] <= list[2] && list[1] >= list[3] {
+      sum = sum + 1
+    }
+  }
+  fmt.Printf("Current sum: %d\n", sum)
+}
+
+func Split(r rune) bool {
+  return r == ',' || r == '-'
+}
--- loader.go
+++ loader.go
@@ -2,10 +2,11 @@ package main
 
 import (
   utils "advent_of_code/utils"
-  day3 "advent_of_code/day3"
+  //day3 "advent_of_code/day3"
+  day4 "advent_of_code/day4"
 )
 
 func Load() {
-  input := utils.LoadData("day3/input.txt")
-  day3.Solution(input);
+  input := utils.LoadData("day4/input.txt")
+  day4.Solution(input);
 }
